84e1122758b709ee39bbcf995a1d831fd87c2788
mu.kumaran1991@gmail.com <mu.kumaran1991@gmail.com>
2025-05-12 18:02:38 +0530

    new commit

--- src/Cart.js
+++ src/Cart.js
@@ -16,8 +16,12 @@ if(isEmpty) return <h1 className='text-center'>your cart is empty</h1>
 
   return (
     <>
+    <div className='row justify-content-center'>
+    <div className='col-6'>
     <button onClick={()=>emptyCart()}>Empty cart</button>
-    <table border='2' align='center'>
+    <h5>Cart:({totalUniqueItems}) Total Items:({totalItemsc})</h5>
+    {/* <table border='2' align='center'> */}
+    <table className='table table-light table-hover m-4'>
         <tbody>
             {
                 items.map((item,index)=>{
@@ -28,6 +32,9 @@ if(isEmpty) return <h1 className='text-center'>your cart is empty</h1>
                             <td>{item.iname}</td>
                             <td>{item.price}</td>
                             <td>{item.quantity}</td>
+                            <td><button className='btn btn-info ms-2' onClick={()=>updateItemQuantity(item.id,item.quantity+1)}>+</button></td>
+                            <td><button className='btn btn-info ms-2' onClick={()=>updateItemQuantity(item.id,item.quantity-1)}>-</button></td>
+                            <td><button className='btn btn-danger ms-2' onClick={()=>removeItem(item.id)}>Remove item</button></td>
                         </tr>
                     )
                 })
@@ -35,6 +42,8 @@ if(isEmpty) return <h1 className='text-center'>your cart is empty</h1>
         </tbody>
 
     </table>
+    </div>
+    </div>
     </>
   )
 }
